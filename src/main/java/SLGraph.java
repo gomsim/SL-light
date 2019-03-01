1a41c46181bbc8b54f98159d2b1edfbd1122a68f
Simon Gombrii <simon@gombrii.se>
2019-03-01 17:18:09 +0100

    stubs SLGraph

--- src/main/java/SLGraph.java
+++ src/main/java/SLGraph.java
@@ -1,2 +1,27 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class SLGraph {
+
+    HashMap<String, Stop> graph = new HashMap<String, Stop>();
+
+    public SLGraph(){
+
+    }
+
+    public boolean addStop(String name, int id, int x, int y){
+        return false;
+    }
+    public boolean remove(String name){
+        return false;
+    }
+    public boolean connect(String start, String dest, int depTime, int arrTime){
+        return false;
+    }
+    public boolean disconnect(String start, String dest, int depTime){
+        return false;
+    }
+    public List<Stop> findRoute(String start, String dest, int time){
+        return null;
+    }
 }
